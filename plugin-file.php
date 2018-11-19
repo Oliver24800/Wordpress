8cc2fbe48b54a6e44a6ff531faeb926f9b5ded67
Oliver24800 <37363787+Oliver24800@users.noreply.github.com>
2018-11-19 12:16:41 +0100

    Add files via upload

---
+++ plugin-file.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Coolste plugin ooit
+Plugin URI:   scargo.net
+Description:  A plugin that does nothing
+Version:      1.0
+Author:       Oliver Scargo
+Author URI:   scargo.net
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
